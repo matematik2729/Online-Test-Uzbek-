7bfb5596b6dbaead7db3448f99f8205b08c0d096
Ulugbek <ulugb3k270@gmail.com>
2022-06-15 19:06:06 +0500

    finished

---
+++ js/savollar.js
@@ -0,0 +1,87 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Bog'lamoq mumkin ammo yechib bo’lmaydi. U nima ?",
+    answer: "Hech qaysisi",
+    options: ["Oyoq kiyim ipi", "Yechilmaydigan ip", "Hech qaysisi"],
+  },
+  {
+    numb: 2,
+    question: "U hamma tilda javob qaytara oladi. Xo’sh nima u ?",
+    answer: "Aks sado.",
+    options: ["Aks sado.", "Yaxshi tarjimon", "google translate:)"],
+  },
+  {
+    numb: 3,
+    question:
+      "O’rta bo’yli erkak qahvaxonaga kirdi va non, qahva,salat va taom buyurtirdi. Ofitsant savol berdi. —siz o’t o’chiruvchimisiz? —ha <> Ofitsant bu kelishgan erkakni o’t o’chiruvchi ekanligini qayerdan bildi?",
+    answer: "Hech qaysi bir to’g’ri emas",
+    options: [
+      "Hech qaysi bir to’g’ri emas",
+      "Gavdali insonligidan",
+      "Oxangidan",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Qozon qanchalik katta bo’lmasin unga nima sig’maydi?      ",
+    answer: "Qopqog’i",
+    options: ["To’g’ri javob yo’q", "Qopqog’i", "Hamma narsa sigadi"],
+  },
+
+  {
+    numb: 5,
+    question: "Qaysi oyda 28 kun bor?",
+    answer: "Hamma oyda",
+    options: ["Hamma oyda.", "Fevral oyida", "4 yilda 3 marta Fevralda"],
+  },
+  {
+    numb: 6,
+    question:
+      "Akbar sakkizga to’lganida otasi 31 yoshda edi. Endi esa otasi Akbardan ikki baravar katta. Xo’sh, Akbar necha yoshga kirdi?",
+    answer: "Akbar 23 yoshda, otasi 46 yoshda;",
+    options: [
+      "Akbar 22 yoshda, otasi 43 yoshda;",
+      "Akbar 23 yoshda, otasi 46 yoshda",
+      "Akbar 24 yoshda, otasi 47 yoshda;",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "Nuriddin doim fudbol boshlanishidan oldin aniq hisobni ayta oladi. Shunaqa ham bo’lishi mumkinmi?    ",
+    answer: "0:0 bo’ladi doim",
+    options: [
+      "0:0 bo’ladi doim",
+      "Sotilgan o’yinlarda faqat",
+      "Yaxshi bashoratchi",
+    ],
+  },
+  {
+    numb: 8,
+    question: "2 litr sutni qanday qilib bir litr bankaga joylash mumkin?    ",
+    answer: "to’g’ri javob yo’q",
+    options: [
+      "sutni bir bankasini to’kib",
+      "to’g’ri javob yo’q",
+      "buni iloji yo’q",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "Savatda 5ta olma bor. Besh kishiga teng tarqatildi. Ammo savatda bitta olma qoldi. Qanday qilib shunday bo’lishi mumkin. ",
+    answer: "To’g’ri javob yo’q",
+    options: [
+      "Bunday bo’lishi mumkin emas",
+      "To’g’ri javob yo’q",
+      "1.5 tadan tarqatgan",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Kim o’tirgan holida yura oldi? ",
+    answer: "Shaxmat o’ynayotgan odam;",
+    options: ["Shaxmat o’ynayotgan odam;", "Nogiron", "To’g’ri javob yo’q"],
+  },
+];
